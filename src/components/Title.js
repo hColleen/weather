9d6d952885c8af53cd05746a59363a07e223fb45
hColleen <hcolleen@gmail.com>
2018-11-07 04:28:54 -0700

    styling updated

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export default class Title extends Component{
     render(){
         return(
         <div>
-            <h1>L<i className ="wi wi-day-sunny"></i>ca<i className = "wi wi-thermometer"></i> Weather</h1>
+            <h1 className="title-container__title">L<i className ="wi wi-day-sunny"></i>ca<i className = "wi wi-thermometer"></i> Weather</h1>
         </div>
         )
     }
