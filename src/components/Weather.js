5a85edfebb2e0f83d537214da45d7e0ea48e494b
hColleen <hcolleen@gmail.com>
2018-11-07 03:41:57 -0700

    add bootstrap

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,24 +3,16 @@ import React from "react";
 const Weather = props => (
     <div className="weather__info">
         {
-            props.city && props.country && <p className="weather__key"> Location:
-	 		<span className="weather__value"> {props.city}, {props.country}</span>
-            </p>
+            props.city && props.country && <p className="weather__key"> Location: <span className="weather__value"> {props.city}, {props.country}</span></p>
         }
         {
-            props.temperature && <p className="weather__key"> Temperature:
-	 		<span className="weather__value"> {props.temperature} &#8451;	</span>
-            </p>
+            props.temperature && <p className="weather__key"> Temperature: <span className="weather__value"> {props.temperature} &#8451;</span></p>
         }
         {
-            props.humidity && <p className="weather__key"> Humidity:
-	 		<span className="weather__value"> {props.humidity}% </span>
-            </p>
+            props.humidity && <p className="weather__key"> Humidity:<span className="weather__value"> {props.humidity}% </span></p>
         }
         {
-            props.description && <p className="weather__key"> Conditions:
-	 		<span className="weather__value"> {props.description} </span>
-            </p>
+            props.description && <p className="weather__key"> Conditions:<span className="weather__value"> {props.description} </span></p>
         }
         {
             props.icon && <p className="weather__key"><i className = {`wi wi-owm-${props.icon}`}></i></p>
