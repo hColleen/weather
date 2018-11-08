bc4ca633f5809efc8ea99bad835f9ed9f5830a1c
hColleen <hcolleen@gmail.com>
2018-11-07 17:35:45 -0700

    Updates

---
+++ precache-manifest.57036712596de49950572de786be4265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "316ab5bceae4fbe33b90e0076bf92186",
+    "url": "/weather/static/media/ouranos.316ab5bc.jpg"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "b723f681e28843759de8",
+    "url": "/weather/static/js/main.b723f681.chunk.js"
+  },
+  {
+    "revision": "24798a5d4788780d4418",
+    "url": "/weather/static/js/1.24798a5d.chunk.js"
+  },
+  {
+    "revision": "b723f681e28843759de8",
+    "url": "/weather/static/css/main.b8345143.chunk.css"
+  },
+  {
+    "revision": "24798a5d4788780d4418",
+    "url": "/weather/static/css/1.c9c96bcc.chunk.css"
+  },
+  {
+    "revision": "fc08ca29e05582690dbb08183dd314ca",
+    "url": "/weather/index.html"
+  }
+];
