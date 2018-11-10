6683a6800406c34f45853988a37d5cf5e960e992
hColleen <hcolleen@gmail.com>
2018-11-10 05:27:09 -0700

    weather chart styled

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 let forecastApi = "https://api.openweathermap.org/data/2.5/forecast?"
 const API_KEY = "a1a849c982e707bbf6fa3890ce6a7add"
 
-export default class FiveDay extends Component{
+export default class FiveDay extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       graphData: {}
@@ -17,8 +17,8 @@ export default class FiveDay extends Component{
     displayLegend: false,
     displayLabel: false
   }
-  
-  
+
+
   componentDidMount() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -33,48 +33,48 @@ export default class FiveDay extends Component{
   getForecast = async () => {
     const api_call = await fetch(`${forecastApi}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
-    let temp = [], icon = [], timestamp =[], dateArray = []
-    for (let i = 0; i < data.list.length; i++){
+    let temp = [], timestamp = [], dateArray = []
+    for (let i = 0; i < data.list.length; i++) {
       temp.push(data.list[i].main.temp)
-      icon.push(data.list[i].weather[0].id)
       timestamp.push(data.list[i].dt)
     }
-    for (let i = 0; i < timestamp.length; i ++){
+    for (let i = 0; i < timestamp.length; i++) {
       let date = new Date(timestamp[i] * 1000)
-      let options = { weekday: 'short', month: 'short'}
+      let options = { weekday: 'short', month: 'short' }
       dateArray.push(date.toLocaleString(options))
     }
-    console.log(dateArray)
+
     this.setState({
-      graphData:{
-    labels: dateArray,
-    datasets:[
-      {
-      label: '',
-      data: temp,
-      backgroundColor:['#001233']
+      graphData: {
+        labels: dateArray,
+        datasets: [
+          {
+            label: 'Forecast temperature',
+            data: temp,
+            fill: 'origin',
+            backgroundColor: ["#04c"]
+          }
+        ]
       }
-    ]
+    })
   }
-  })
-  }
-  
+
   render() {
     return (
-     <div className = "forecast_graph">
-      <h3 className = "forecast_title">Five Day Forecast</h3>
-      <Line 
-        data = {this.state.graphData}
-        options = {{
-          title: {
-            display: this.props.displayTitle
-          },
-          legend:{
-            display: this.props.displayLegend
-          }
-        }}
-      />
-     </div> 
+      <div className="forecast_graph">
+        <h3 className="forecast_title">Five Day Forecast</h3>
+        <Line
+          data={this.state.graphData}
+          options={{
+            title: {
+              display: this.props.displayTitle
+            },
+            legend: {
+              display: this.props.displayLegend
+            }
+          }}
+        />
+      </div>
     )
   }
 }
